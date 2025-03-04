1017f9493ed801acd215c8b0fd10ba24ea568fd4
AliKhachab <a.khachab620@gmail.com>
2025-03-04 17:32:33 -0500

    lock on number practice

--- src/CountButton.jsx
+++ src/CountButton.jsx
@@ -1,14 +1,18 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
-export default function CountButton({ type, setCount }) {
+export default function CountButton({ type, setCount, locked }) {
   const handleClick = (type) => {
     if (type === "plus") {
-      setCount((prev) => prev + 1);
+      setCount((prev) => {
+        const newCount = prev + 1;
+        return newCount > 5 ? 5 : newCount; // return 5 if newCount is greater than 5 else return newCount
+      });
     } else {
       setCount((prev) => (prev > 0 ? prev - 1 : 0));
     }
   };
   return (
     <button
+      disabled={locked}
       className="count-btn"
       onClick={() => {
         handleClick(type);
--- src/Title.jsx
+++ src/Title.jsx
@@ -1,5 +1,9 @@
-export default function Title() {
+export default function Title({ locked }) {
   return (
-    <h1 className="title">Fancy Counter</h1>
+    <h1 className="title">{
+      locked ? 
+      <span>Limit! buy <b>PRO</b> for &gt;5</span>
+      : "Fancy Counter"
+    }</h1>
   )
 }
